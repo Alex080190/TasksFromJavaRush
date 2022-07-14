fc018136e7b9dff11af786285e946c5a37bb56c4
Alex080190 <Alex080190@mail.ru>
2022-07-14 21:36:41 +0300

    JavaRush

---
+++ src/Arrays/CreatingArrays.java
@@ -0,0 +1,8 @@
+package Arrays;
+
+public class CreatingArrays {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
